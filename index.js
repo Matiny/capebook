b9e79806c4a4d25933908702ae1d6fc7b1bc6e3c
Matiny <matinylouis@gmail.com>
2018-08-11 20:20:51 -0400

    Deploying to Heroku

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bparser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
@@ -31,6 +33,16 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  //Now create the catch all route
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
